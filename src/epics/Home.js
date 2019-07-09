dd29b5ec1f68994a5e06d1e408138d2237d69495
storrellas <storrellas@gmail.com>
2019-07-09 23:03:10 +0000

    ENH: Adding modal

--- src/epics/Home.js
+++ src/epics/Home.js
@@ -5,13 +5,27 @@ import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { shadows } from '@material-ui/system';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
 import Typography from '@material-ui/core/Typography';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 
+import Avatar from '@material-ui/core/Avatar';
+import PersonIcon from '@material-ui/icons/Person';
+import AddIcon from '@material-ui/icons/Add';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+
 import Box from '@material-ui/core/Box';
 
+import { makeStyles } from '@material-ui/core/styles';
 
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -50,7 +64,7 @@ const styles = theme => ({
   },
   titlePosition: {
     position: 'absolute', 
-    width:'200px', 
+    width:'45%', 
     top: 30, 
     margin: 'auto', 
     left: 0, 
@@ -83,17 +97,89 @@ const styles = theme => ({
   icon: {
     textAlign: 'center',
     fontWeight: 'bold',
-  }  
+  },
+  iconButton: {
+    borderRadius:'50%', 
+    backgroundColor: 'red', 
+    margin: 24
+  }
 });
+
+const emails = ['username@gmail.com', 'user02@gmail.com'];
+
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: 'red',
+    color: 'blue',
+  },
+});
+
+function SimpleDialog(props) {
+  const classes = useStyles();
+  const { onClose, selectedValue, ...other } = props;
+
+  function handleClose() {
+    onClose(selectedValue);
+  }
+
+  function handleListItemClick(value) {
+    onClose(value);
+  }
+
+  return (
+    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" {...other}>
+      <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+      <List>
+        {emails.map(email => (
+          <ListItem button onClick={() => handleListItemClick(email)} key={email}>
+            <ListItemAvatar>
+              <Avatar className={classes.avatar}>
+                <PersonIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={email} />
+          </ListItem>
+        ))}
+
+        <ListItem button onClick={() => handleListItemClick('addAccount')}>
+          <ListItemAvatar>
+            <Avatar>
+              <AddIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="add account" />
+        </ListItem>
+      </List>
+    </Dialog>
+  );
+}
+
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func,
+  open: PropTypes.bool,
+  selectedValue: PropTypes.string,
+};
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      open: false,
     };
 
   }
+
+  handleClick(e){
+    e.preventDefault();
+    console.log('The link was clicked.');
+    this.setState({open: true})
+  }
+
+  handleClose(){
+    console.log('The link was closed')
+    this.setState({open: false})
+  };
 
   render() {
     const { classes } = this.props;
@@ -114,33 +200,38 @@ class Home extends React.Component {
 
         <Grid container spacing={3} className={classNames(classes.iconContainer)}>
           <Grid item xs className={classNames(classes.icon)}>
-            <Box mt={2} ml={3} mr={3} p={1} style={{backgroundColor:'white'}}  borderRadius="50%">
-              <img width="100%" height="100%" src={undoImage}></img>
+            <Box mt={2} ml={3} mr={3} p={1} style={{backgroundColor:'white'}}  
+                borderRadius="50%" onClick={(e) => this.handleClick(e)}>
+              <img width="100%" height="100%" src={undoImage}></img>  
             </Box>
             <p>Undo</p>
           </Grid>
           <Grid item xs className={classNames(classes.icon)}>
-            <Box mt={6} ml={3} mr={3} p={1} style={{backgroundColor:'white'}}  borderRadius="50%">
+            <Box mt={6} ml={3} mr={3} p={1} style={{backgroundColor:'white'}}  
+                borderRadius="50%" onClick={(e) => this.handleClick(e)}>
               <img width="100%" height="100%" src={noInterestImage}></img>
             </Box>
             <p>No interest</p>
           </Grid>
           <Grid item xs className={classNames(classes.icon)}>
-            <Box mt={6} ml={3} mr={3} p={1} style={{backgroundColor:'white'}}  borderRadius="50%">
+            <Box mt={6} ml={3} mr={3} p={1} style={{backgroundColor:'white'}}  
+                borderRadius="50%" onClick={(e) => this.handleClick(e)}>
               <img width="100%" height="100%" src={likeToTryImage}></img>
             </Box>
             <p>Like to try</p>
           </Grid>
           <Grid item xs className={classNames(classes.icon)}>
-            <Box mt={2} ml={3} mr={3} p={1} style={{backgroundColor:'white'}} borderRadius="50%">
-              <img width="100%" height="100%" src={alreadyPlayedImage} style={{borderRadius: '50%'}}></img>
+            <Box mt={2} ml={3} mr={3} p={1} style={{backgroundColor:'white'}} 
+                borderRadius="50%" onClick={(e) => this.handleClick(e)}>
+              <img width="100%" height="100%" src={alreadyPlayedImage}></img>
             </Box>
             <p>Play Already</p>
-
           </Grid>
         </Grid>            
-        <Box mt={5} ml={3} mr={3}>
-        </Box>
+
+
+        <SimpleDialog selectedValue="MyTest" open={this.state.open} onClose={(e) => this.handleClose()} />
+
       </Container>
     );
   }
