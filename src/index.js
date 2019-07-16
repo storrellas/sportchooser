e32a91be1eb29ab92bd36c4fbc961e3da8375e64
storrellas <storrellas@gmail.com>
2019-07-16 09:47:41 +0000

    ENH: Removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import Landing from "./epics/Landing.js";
 import Home from "./epics/Home.js";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import Startup from "./epics/Startup.js"
@@ -8,21 +7,6 @@ import Startup from "./epics/Startup.js"
 // Redux
 import { Provider } from "react-redux";
 import store from "./redux";
-
-
-/*
-ReactDOM.render((
-  <BrowserRouter>
-    <div>      
-      <Route path="/" exact component={Login} />
-      <Route path="/map/" component={Map} />
-      <Route path="/chart/" component={Chart} />
-    </div>
-  </BrowserRouter>
-), document.getElementById('root'))
-/**/
-
-
 
 ReactDOM.render((
   <Provider store={store}>
