b4aa2890d8d0af21b10619e1b1814f92a44ea1b0
storrellas <storrellas@gmail.com>
2019-07-24 14:01:42 +0000

    ENH: FriendCard with Grid

--- src/components/FriendCard.js
+++ src/components/FriendCard.js
@@ -9,7 +9,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+
 
 
 // Project imports
@@ -71,7 +72,7 @@ class MediaCard extends React.Component {
             </Typography>
           </Box>
           <CardContent classes={{ root: classes.content }} style={{ width: "100%"}}>
-            <Box pt={2} pl={1} pr={1} m={0} style={{ display: 'flex', width:"100%", height: "5em" }}>
+            {/* <Box pt={2} pl={1} pr={1} m={0} style={{ display: 'flex', width:"100%", height: "5em" }}>
               <div style={{ height: "100%", borderRight: "1px solid black", paddingRight: 10}}>
                 <div>Wants to Try</div>
                 <div style={{ display: 'flex', height: "80%"}}>
@@ -86,7 +87,38 @@ class MediaCard extends React.Component {
                   <img height="100%" src="http://3.121.215.237/media/fixture/icon_swimming.png" />
                 </div>
               </div>
-            </Box>
+            </Box> */}
+
+            <Box pt={2} pl={1} pr={1} m={0} style={{ display: 'flex', width:"100%"}}>
+              <Grid container spacing={3} style={{ backgroundColor: 'blue'}}>
+                  <Grid item xs={6} style={{ backgroundColor: 'green'}}>
+                    <div style={{ height: "20%" }}>Wants to Try</div>
+
+                    <Grid container style={{ height: "80%"}}>
+                      <Grid item xs={6}>
+                        <img width="100%" src="http://3.121.215.237/media/fixture/icon_judo.png" />
+                      </Grid>
+                      <Grid item xs={6}>
+                        <img width="100%" src="http://3.121.215.237/media/fixture/icon_rugby.png" />
+                      </Grid>
+                    </Grid>
+
+                  </Grid>
+                  <Grid item xs={6} style={{ backgroundColor: 'yellow'}}>
+                    <div>AlreadyPlayed</div>
+
+                    <Grid container style={{ height: "80%"}}>
+                      <Grid item xs={6}>
+                        <img width="100%" src="http://3.121.215.237/media/fixture/icon_running.png" />
+                      </Grid>
+                      <Grid item xs={6}>
+                        <img width="100%" src="http://3.121.215.237/media/fixture/icon_swimming.png" />
+                      </Grid>
+                    </Grid>
+
+                  </Grid>
+                </Grid>
+              </Box>
           </CardContent>
         </CardActionArea>
       </Card>
